95faad2a096eec10185d366b5af50e5500724c2e
Amber Lynn <griffiam@gmail.com>
2018-09-27 20:07:40 -0700

    The time complexity is O(n) because with each iteration, the size of the does not increase. The space complexity checks through elements with each iteration n times - O(n).

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,59 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
-  raise NotImplementedError
+  #   if my_phrase == nil || my_phrase[0] == " " || my_phrase[-1] == " "
+  #     return false
+  #   elsif
+  #     my_phrase == "" || my_phrase.length == 1
+  #     return true
+  #   else
+  #     start = 0
+  #     ender = my_phrase.length - 1
+  #
+  #     while start < ender
+  #       if my_phrase[start] == " " || my_phrase[ender] == " "
+  #         if my_phrase[start] == " "
+  #           start += 1
+  #         end
+  #         if my_phrase[ender] == " "
+  #           ender -= 1
+  #         end
+  #       end
+  #       if my_phrase[start] == my_phrase[ender]
+  #         start += 1
+  #         ender -= 1
+  #       else
+  #         return false
+  #       end
+  #     end
+  #   end
+  #   return true
+  # end
+
+  if my_phrase == nil
+    return false
+  elsif
+    my_phrase == "" || my_phrase.length == 1
+    return true
+  else
+    start = 0
+    ender = my_phrase.length - 1
+
+    while start < ender
+      if my_phrase[start] == " "
+        start += 1
+
+        if my_phrase[ender] == " "
+          ender -= 1
+        end
+      end
+
+      if my_phrase[start] != my_phrase[ender]
+        return false
+      else
+        return true
+      end
+    end
+  end
+  return true
 end
